282a145de3b035048bd0828b7fa3d79d698c0660
dthompson1411 <dthompson1411@gmail.com>
2018-08-25 12:22:05 +0800

    Program for reading and writing data to a file

--- src/filemanipulation/Execute.java
+++ src/filemanipulation/Execute.java
@@ -32,16 +32,29 @@ public class Execute
           //prompt user for a file name
           System.out.printf("Please enter a File Name:\n");
           FileName = input.next();
-          System.out.printf("The data stored in the Variable is %s", FileName);
+          System.out.printf("The data stored in the Variable is %s:\n", FileName);
           //Add the text extension to variable
           myFile.Name = FileName.concat(".txt");
+          System.out.printf("The value stored in the Class is %s. \n", myFile.getName());
           
           //Prompt for data Input
           System.out.printf("Please enter a series of Numbers separated by a comma.\n");
           Data = input.next();
+          System.out.printf("The data stored in the variable is %s. \n", Data);
           //Add data to class
           myFile.Data = Data;
+          System.out.printf("The value stored in the class is %s. \n", myFile.getData());
           
+          System.out.printf("Writing the data to the filename %s.\n", myFile.getName());
+          
+          //Writing Data to File
+          WriteFile.Write(myFile);
+          
+          //Reading Data from file and Displaying it
+          System.out.printf("Reading the file and printing it to the screen\n");
+          
+          //Reading file and displaying it on the screen
+          ReadFile.Read(myFile);
           
     
           
